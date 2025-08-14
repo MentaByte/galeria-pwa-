7fe9a67a39158f2e5286fd2a35943d927f1b14f8
MentaByte <nabilft@gmail.com>
2025-08-13 22:06:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,29 @@
-const CACHE_NAME = 'galeria-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'galeria-v2';
+const ASSETS = [
   '/',
   '/index.html',
   'https://i.imgur.com/NTpM7zB.jpg',
   'https://i.imgur.com/8NB2kyD.jpg',
-  // Añade aquí todas las URLs de imágenes que quieres cachear
+  'https://i.imgur.com/cLVENtZ.jpg',
+  'https://i.imgur.com/N5Dw0uI.jpg',
+  'https://i.imgur.com/jibpP7q.jpg',
+  'https://i.imgur.com/ZS1JtFE.jpg',
+  'https://i.imgur.com/bDjSxj7.jpg',
+  'https://i.imgur.com/xbHO4ET.jpg',
+  'https://i.imgur.com/752qncM.jpg',
+  'https://i.imgur.com/CDkvzSZ.jpg'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
